87b24c3d53fc8df6c149246dc399eed2a2ea16f3
Nikita <nicandreo@gmail.com>
2022-05-30 13:20:08 +0300

    Initial Commit

---
+++ OmegaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmegaTest
+//
+//  Created by Nikita Batrakov on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
